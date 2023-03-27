4812f532d588a180f8099c4ca159fa68b3d073a1
Sunjeet Khokhaer <sunjeet81@gmail.com>
2023-03-27 13:14:40 +1300

    combine into a single test

--- tests/Post_Login.spec.ts
+++ tests/Post_Login.spec.ts
@@ -4,12 +4,10 @@ import { test, expect } from '@playwright/test';
   await page.goto('https://trademe.co.nz');
 });*/
 
-test('user already logged in', async ({ page }) => {
+test('logout if user already logged in', async ({ page }) => {
   await page.goto('https://trademe.co.nz');
   await expect(page.getByRole('link', { name: 'Log out' }).first()).toBeVisible();
-})
-
-test('log out now', async ({ page }) => {
   await page.getByRole('link', { name: 'Log out' }).first().click();
 })
+
 
