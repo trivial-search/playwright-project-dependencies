d27d7f74a8a33ba618fc01c3ed988921fad307d2
Sunjeet Khokhaer <sunjeet81@gmail.com>
2023-03-28 11:50:47 +1300

    two exclamantions in password

--- tests/TradeMe.login.setup.ts
+++ tests/TradeMe.login.setup.ts
@@ -9,7 +9,7 @@ setup('perform login', async ({ page }) => {
   await page.frameLocator('internal:role=dialog >> iframe').getByLabel('Email').click();
   await page.frameLocator('internal:role=dialog >> iframe').getByLabel('Email').fill(process.env.USERNAME!);
   await page.frameLocator('internal:role=dialog >> iframe').getByLabel('Email').press('Tab');
-  await page.frameLocator('internal:role=dialog >> iframe').getByLabel('Password').fill(process.env.PASSWORD!!);
+  await page.frameLocator('internal:role=dialog >> iframe').getByLabel('Password').fill(process.env.PASSWORD!);
   await page.frameLocator('internal:role=dialog >> iframe').getByLabel('Password').press('Enter');
 
   await expect(page.getByRole('link', { name: 'Log out' }).first()).toBeVisible();
