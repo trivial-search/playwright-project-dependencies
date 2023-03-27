ffb17235758c7681e74ce9954d4df7a7cf68bfb1
Sunjeet Khokhaer <sunjeet81@gmail.com>
2023-03-28 12:55:08 +1300

    only have logout test for validation that state has been passed

--- tests/Post_Login.spec.ts
+++ tests/Post_Login.spec.ts
@@ -4,17 +4,6 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
-test('menu', async ({ page }) => {
-  await page.getByRole('link', { name: 'TestingLogin' }).click();
-  await expect(page.getByRole('heading', { name: /TestingLogin/i })).toBeVisible();
-  await page.getByRole('link', { name: /alerts/i  }).click();
-  await page.getByText('Alerts', { exact: true }).click();
-  await page.getByRole('button', { name: /notice/i  }).click();
-  await page.getByText('Notices').click();
-  await page.getByRole('link', { name: /watchlist/i  }).click();
-
-})
-
 test('logs user out', async ({ page }) => {
   await page.getByRole('button', { name: /Personal tools/i }).check();
   await page.getByRole('link', { name:  /Log out/i }).click();
