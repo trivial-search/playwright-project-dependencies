e2fabc57f85495233042a5958cd775bfa2d3cb98
Sunjeet Khokhaer <sunjeet81@gmail.com>
2023-03-28 11:22:29 +1300

    remove comments

--- tests/Post_Login.spec.ts
+++ tests/Post_Login.spec.ts
@@ -1,10 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-/*test.beforeEach(async ({ page }) => {
-  await page.goto('https://trademe.co.nz');
-});*/
-
-test('logout if user already logged in', async ({ page }) => {
+test('are you logged in?', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByRole('link', { name: 'Log out' }).first()).toBeVisible();
   await page.getByRole('link', { name: 'Log out' }).first().click();
